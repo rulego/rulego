3c02126c09e2c798041089bb59728668d0caef87
rulego-team <rulego@outlook.com>
2023-08-25 16:07:29 +0800

    feat:add plugin examples

---
+++ examples/plugin/plugin.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"github.com/rulego/rulego/api/types"
+	"strings"
+	"time"
+)
+
+//go build -buildmode=plugin -o plugin.so plugin.go # Compile the plugin and generate the plugin.so file
+//need to compile in mac or linux environment
+
+//Plugins plugin entry point
+var Plugins MyPlugins
+
+type MyPlugins struct{}
+
+func (p *MyPlugins) Init() error {
+	return nil
+}
+func (p *MyPlugins) Components() []types.Node {
+	return []types.Node{&UpperNode{}, &TimeNode{}, &FilterNode{}}
+}
+
+//UpperNode A plugin that converts the message data to uppercase
+type UpperNode struct{}
+
+func (n *UpperNode) Type() string {
+	return "test/upper"
+}
+func (n *UpperNode) New() types.Node {
+	return &UpperNode{}
+}
+func (n *UpperNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	// Do some initialization work
+	return nil
+}
+
+func (n *UpperNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) error {
+	msg.Data = strings.ToUpper(msg.Data)
+	// Send the modified message to the next node
+	ctx.TellSuccess(msg)
+	return nil
+}
+
+func (n *UpperNode) Destroy() {
+	// Do some cleanup work
+}
+
+//TimeNode A plugin that adds a timestamp to the message metadata
+type TimeNode struct{}
+
+func (n *TimeNode) Type() string {
+	return "test/time"
+}
+
+func (n *TimeNode) New() types.Node {
+	return &TimeNode{}
+}
+
+func (n *TimeNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	// Do some initialization work
+	return nil
+}
+
+func (n *TimeNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) error {
+	msg.Metadata.PutValue("timestamp", time.Now().Format(time.RFC3339))
+	// Send the modified message to the next node
+	ctx.TellSuccess(msg)
+	return nil
+}
+
+func (n *TimeNode) Destroy() {
+	// Do some cleanup work
+}
+
+type FilterNode struct {
+	blacklist map[string]bool
+}
+
+func (n *FilterNode) Type() string {
+	return "test/filter"
+}
+func (n *FilterNode) New() types.Node {
+	return &FilterNode{}
+}
+func (n *FilterNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	// Do some initialization work
+	return nil
+}
+
+func (n *FilterNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) error {
+	if n.blacklist[msg.Type] || n.blacklist[string(msg.DataType)] {
+		// Skip the message and do not send it to the next node
+		return nil
+	}
+	// Send the message to the next node
+	ctx.TellNext(msg)
+	return nil
+}
+
+func (n *FilterNode) Destroy() {
+	// Do some cleanup work
+}
---
+++ examples/plugin/plugin_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/rulego/rulego"
+	"github.com/rulego/rulego/api/types"
+	string2 "github.com/rulego/rulego/utils/str"
+	"time"
+)
+
+var (
+	shareKey   = "shareKey"
+	shareValue = "shareValue"
+)
+
+//测试插件，需要在linux运行
+//先编译plugin.go
+//go build -buildmode=plugin -o plugin.so plugin.go
+func main() {
+	_ = rulego.Registry.Unregister("test")
+	//注册插件组件
+	err := rulego.Registry.RegisterPlugin("test", "./plugin.so")
+	if err != nil {
+		panic(err)
+	}
+	config := rulego.NewConfig()
+
+	ruleEngine, err := rulego.New(string2.RandomStr(10), []byte(chainJsonFile), rulego.WithConfig(config))
+	metaData := types.BuildMetadata(make(map[string]string))
+	metaData.PutValue("productType", "test01")
+	msg := types.NewMsg(0, "TEST_MSG_TYPE", types.JSON, metaData, "aa")
+
+	ruleEngine.OnMsgWithOptions(msg, types.WithContext(context.WithValue(context.Background(), shareKey, shareValue)), types.WithEndFunc(func(msg types.RuleMsg, err error) {
+		fmt.Println(msg)
+	}))
+
+	time.Sleep(time.Second * 1)
+}
+
+var chainJsonFile = `
+	{
+	  "ruleChain": {
+		"name": "测试规则链",
+		"root": true
+	  },
+	  "metadata": {
+		"nodes": [
+		  {
+			"id":"s1",
+			"type": "test/upper",
+			"name": "转大写",
+			"debugMode": true
+		  },
+		  {
+			"id":"s2",
+			"type": "test/time",
+			"name": "增加时间",
+			"debugMode": true
+		  }
+		],
+		"connections": [
+		  {
+			"fromId": "s1",
+			"toId": "s2",
+			"type": "Success"
+		  }
+		],
+		"ruleChainConnections": null
+	  }
+	}
+`
