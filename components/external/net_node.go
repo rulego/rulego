83b463989093d2bcb625b95eca47bd9752ea88e4
rulego-team <rulego@outlook.com>
2023-11-08 22:43:57 +0800

    feat:add net component

---
+++ components/external/net_node.go
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2023 The RuleGo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package external 把msg负荷发送到指定协议网络服务器（不支持读取数据），支持协议：tcp、udp、ip4:1、ip6:ipv6-icmp、ip6:58、unix、unixgram，以及net包支持的协议类型。
+//每条消息在内容最后增加结束符：'\n'
+package external
+
+import (
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/utils/maps"
+	"net"
+	"sync/atomic"
+	"time"
+)
+
+//EndSign 结束符
+const EndSign = '\n'
+
+//注册节点
+func init() {
+	Registry.Add(&NetNode{})
+}
+
+// NetNodeConfiguration 组件的配置
+type NetNodeConfiguration struct {
+	// 通信协议，可以是tcp、udp、ip4:1、ip6:ipv6-icmp、ip6:58、unix、unixgram，以及net包支持的协议类型。默认tcp协议
+	Protocol string
+	// 服务器的地址，格式为host:port
+	Server string
+	// 连接超时，单位为秒，如果<=0 则默认60
+	ConnectTimeout int
+	// 心跳间隔，用于定期发送心跳消息，单位为秒，如果<=0 则默认60
+	HeartbeatInterval int
+}
+
+// NetNode 用于网络协议的数据读取和发送，支持协议：tcp、udp、ip4:1、ip6:ipv6-icmp、ip6:58、unix、unixgram，以及net包支持的协议类型。
+type NetNode struct {
+	// 节点配置
+	Config NetNodeConfiguration
+	// ruleGo配置
+	ruleConfig types.Config
+	// 客户端连接对象
+	conn net.Conn
+	// 创建一个心跳定时器，用于定期发送心跳消息，可以为0表示不发心跳
+	heartbeatTimer *time.Timer
+	//心跳间隔
+	heartbeatDuration time.Duration
+	// 用于通知组件已经被销毁
+	stop chan struct{}
+	// 连接是否已经断开，0：没端口；1：端口
+	disconnected int32
+}
+
+//Type 组件类型
+func (x *NetNode) Type() string {
+	return "net"
+}
+
+func (x *NetNode) New() types.Node {
+	return &NetNode{Config: NetNodeConfiguration{
+		Protocol:          "tcp",
+		ConnectTimeout:    60,
+		HeartbeatInterval: 60,
+	}}
+}
+
+//Init 初始化
+func (x *NetNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	x.ruleConfig = ruleConfig
+	x.stop = make(chan struct{}, 1)
+	// 将配置转换为NetNodeConfiguration结构体
+	err := maps.Map2Struct(configuration, &x.Config)
+	if err == nil {
+		if x.Config.Protocol == "" {
+			x.Config.Protocol = "tcp"
+		}
+		if x.Config.ConnectTimeout == 0 {
+			x.Config.ConnectTimeout = 60
+		}
+		if x.Config.HeartbeatInterval == 0 {
+			x.Config.HeartbeatInterval = 60
+		}
+		x.heartbeatDuration = time.Duration(x.Config.HeartbeatInterval) * time.Second
+
+		// 根据配置的协议和地址，创建一个客户端连接
+		err = x.onConnect()
+		//启动ping、重连和读取服务端数据
+		if err == nil {
+			go x.start()
+		}
+	}
+	return err
+}
+
+//OnMsg 处理消息
+func (x *NetNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) error {
+	// 将消息的数据转换为字节数组
+	data := []byte(msg.Data)
+	// 在数据的末尾加上结束符
+	data = append(data, EndSign)
+	return x.onWrite(ctx, msg, data)
+}
+
+//Destroy 销毁
+func (x *NetNode) Destroy() {
+	// 发送一个空结构体到chain
+	x.stop <- struct{}{}
+
+	close(x.stop)
+}
+
+// start 启动
+func (x *NetNode) start() {
+
+	// 循环处理服务器的数据和事件
+LOOP:
+	for {
+		// 使用select语句，监听多个通道
+		select {
+		// 如果接收到stop的消息
+		case <-x.stop:
+			x.onDisconnect()
+			// 退出for循环
+			break LOOP
+		// 如果有服务器发送的数据
+		default:
+			if x.isDisconnected() {
+				_ = x.onConnect()
+				//5秒后重连
+				time.Sleep(time.Second * 5)
+			} else {
+				time.Sleep(x.heartbeatDuration)
+			}
+
+		}
+	}
+}
+
+func (x *NetNode) Printf(format string, v ...interface{}) {
+	x.ruleConfig.Logger.Printf(format, v...)
+}
+
+func (x *NetNode) onConnect() error {
+	//如果有旧连接则先关闭
+	if x.conn != nil {
+		_ = x.conn.Close()
+	}
+	// 根据配置的协议和地址，创建一个客户端连接
+	conn, err := net.DialTimeout(x.Config.Protocol, x.Config.Server, time.Duration(x.Config.ConnectTimeout)*time.Second)
+	if err == nil {
+		x.conn = conn
+		x.setDisconnected(false)
+
+		if x.heartbeatTimer != nil {
+			x.heartbeatTimer.Reset(x.heartbeatDuration)
+		} else {
+			// 创建一个心跳定时器，用于定期发送心跳消息
+			x.heartbeatTimer = time.AfterFunc(x.heartbeatDuration, func() {
+				x.onPing()
+			})
+		}
+		x.Printf("connect success:", x.conn.RemoteAddr().String())
+	} else {
+		x.Printf("connect err:", err)
+	}
+	return err
+}
+
+func (x *NetNode) onPing() {
+	// 如果连接已经断开，就跳过
+	if x.isDisconnected() {
+		return
+	}
+	// 向服务器发送心跳消息
+	_, err := x.conn.Write([]byte("ping\n"))
+	if err != nil {
+		// 如果出现错误，就标记连接断开，并重置重连定时器
+		x.setDisconnected(true)
+	} else {
+		x.heartbeatTimer.Reset(x.heartbeatDuration)
+	}
+}
+
+func (x *NetNode) onWrite(ctx types.RuleContext, msg types.RuleMsg, data []byte) error {
+	// 向服务器发送数据
+	_, err := x.conn.Write(data)
+	if err != nil {
+		ctx.TellFailure(msg, err)
+	} else {
+		//重置心跳发送间隔
+		if x.heartbeatTimer != nil {
+			x.heartbeatTimer.Reset(x.heartbeatDuration)
+		}
+		//发送到下一个阶段
+		ctx.TellSuccess(msg)
+	}
+	return err
+}
+
+func (x *NetNode) onDisconnect() {
+	if x.conn != nil {
+		_ = x.conn.Close()
+	}
+	// 停止心跳定时器
+	x.heartbeatTimer.Stop()
+	x.setDisconnected(true)
+	x.Printf("onDisconnect " + x.conn.RemoteAddr().String())
+}
+
+func (x *NetNode) isDisconnected() bool {
+	return atomic.LoadInt32(&x.disconnected) == 1
+}
+
+func (x *NetNode) setDisconnected(disconnected bool) {
+	if disconnected {
+		atomic.StoreInt32(&x.disconnected, 1)
+	} else {
+		atomic.StoreInt32(&x.disconnected, 0)
+	}
+}
---
+++ endpoint/net/net.go
@@ -0,0 +1,396 @@
+/*
+ * Copyright 2023 The RuleGo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/endpoint"
+	"github.com/rulego/rulego/test/assert"
+	"github.com/rulego/rulego/utils/maps"
+	"log"
+	"net"
+	"net/textproto"
+	"os"
+	"regexp"
+	"time"
+)
+
+//RequestMessage 请求消息
+type RequestMessage struct {
+	conn net.Conn
+	body []byte
+	msg  *types.RuleMsg
+	err  error
+}
+
+func (r *RequestMessage) Body() []byte {
+	return r.body
+}
+
+func (r *RequestMessage) Headers() textproto.MIMEHeader {
+	headers := make(textproto.MIMEHeader)
+	headers.Set("remoteAddr", r.conn.RemoteAddr().String())
+	return make(textproto.MIMEHeader)
+}
+
+//From 返回客户端Addr
+func (r RequestMessage) From() string {
+	return r.conn.RemoteAddr().String()
+}
+
+func (r *RequestMessage) GetParam(key string) string {
+	return ""
+}
+
+func (r *RequestMessage) SetMsg(msg *types.RuleMsg) {
+	r.msg = msg
+}
+
+func (r *RequestMessage) GetMsg() *types.RuleMsg {
+	if r.msg == nil {
+		dataType := types.TEXT
+		ruleMsg := types.NewMsg(0, r.From(), dataType, types.NewMetadata(), string(r.Body()))
+		r.msg = &ruleMsg
+	}
+	return r.msg
+}
+
+func (r *RequestMessage) SetStatusCode(statusCode int) {
+}
+
+func (r *RequestMessage) SetBody(body []byte) {
+	r.body = body
+}
+
+func (r *RequestMessage) SetError(err error) {
+	r.err = err
+}
+
+func (r *RequestMessage) GetError() error {
+	return r.err
+}
+
+func (r *RequestMessage) Conn() net.Conn {
+	return r.conn
+}
+
+//ResponseMessage 响应消息
+type ResponseMessage struct {
+	conn net.Conn
+	log  func(format string, v ...interface{})
+	body []byte
+	msg  *types.RuleMsg
+	err  error
+}
+
+func (r *ResponseMessage) Body() []byte {
+	return r.body
+}
+
+func (r *ResponseMessage) Headers() textproto.MIMEHeader {
+	headers := make(textproto.MIMEHeader)
+	headers.Set("remoteAddr", r.conn.RemoteAddr().String())
+	return make(textproto.MIMEHeader)
+}
+
+func (r *ResponseMessage) From() string {
+	return r.conn.RemoteAddr().String()
+}
+
+func (r *ResponseMessage) GetParam(key string) string {
+	return ""
+}
+
+func (r *ResponseMessage) SetMsg(msg *types.RuleMsg) {
+	r.msg = msg
+}
+func (r *ResponseMessage) GetMsg() *types.RuleMsg {
+	return r.msg
+}
+
+func (r *ResponseMessage) SetStatusCode(statusCode int) {
+}
+
+func (r *ResponseMessage) SetBody(body []byte) {
+	_, err := r.conn.Write(body)
+	if err != nil {
+		log.Println("write err:", err)
+	}
+}
+
+func (r *ResponseMessage) SetError(err error) {
+	r.err = err
+}
+
+func (r *ResponseMessage) GetError() error {
+	return r.err
+}
+
+// Config endpoint组件的配置
+type Config struct {
+	// 通信协议，可以是tcp、udp、ip4:1、ip6:ipv6-icmp、ip6:58、unix、unixgram，以及net包支持的协议类型。默认tcp协议
+	Protocol string
+	// 服务器的地址，格式为host:port
+	Addr string
+	// 读取超时，用于设置读取数据的超时时间，单位为秒，可以为0表示不设置超时
+	ReadTimeout int
+}
+
+//RegexpRouter 正则表达式路由
+type RegexpRouter struct {
+	//路由
+	router *endpoint.Router
+	//正则表达式
+	regexp *regexp.Regexp
+}
+
+// Endpoint net endpoint组件
+type Endpoint struct {
+	// 嵌入endpoint.BaseEndpoint，继承其方法
+	endpoint.BaseEndpoint
+	// 配置
+	Config Config
+	// rulego配置
+	RuleConfig types.Config
+	// 服务器监听器对象
+	listener net.Listener
+	// 路由映射表
+	routers map[string]*RegexpRouter
+}
+
+// Type 组件类型
+func (ep *Endpoint) Type() string {
+	return "net"
+}
+
+func (ep *Endpoint) New() types.Node {
+	return &Endpoint{
+		Config: Config{Protocol: "tcp", ReadTimeout: 60},
+	}
+}
+
+// Init 初始化
+func (ep *Endpoint) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	// 将配置转换为EndpointConfiguration结构体
+	err := maps.Map2Struct(configuration, &ep.Config)
+	if ep.Config.Protocol == "" {
+		ep.Config.Protocol = "tcp"
+	}
+	ep.RuleConfig = ruleConfig
+	return err
+}
+
+// Destroy 销毁
+func (ep *Endpoint) Destroy() {
+	_ = ep.Close()
+}
+
+func (ep *Endpoint) Close() error {
+	if nil != ep.listener {
+		return ep.listener.Close()
+	}
+	return nil
+}
+
+func (ep *Endpoint) Id() string {
+	return ep.Config.Addr
+}
+
+func (ep *Endpoint) AddRouterWithParams(router *endpoint.Router, params ...interface{}) (string, error) {
+	if router == nil {
+		return "", errors.New("router can not nil")
+	} else {
+		expr := router.GetFrom().From
+		//允许空expr，表示匹配所有
+		var regexpV *regexp.Regexp
+		if expr != "" {
+			//编译表达式
+			if re, err := regexp.Compile(expr); err != nil {
+				return "", err
+			} else {
+				regexpV = re
+			}
+		}
+		ep.Lock()
+		defer ep.Unlock()
+		if ep.routers == nil {
+			ep.routers = make(map[string]*RegexpRouter)
+		}
+		if _, ok := ep.routers[expr]; ok {
+			return expr, fmt.Errorf("duplicate router %s", expr)
+		} else {
+			ep.routers[expr] = &RegexpRouter{
+				router: router,
+				regexp: regexpV,
+			}
+
+			return expr, nil
+		}
+
+	}
+}
+
+func (ep *Endpoint) RemoveRouterWithParams(routerId string, params ...interface{}) error {
+	ep.Lock()
+	defer ep.Unlock()
+	if ep.routers != nil {
+		delete(ep.routers, routerId)
+	}
+	return nil
+}
+
+func (ep *Endpoint) Start() error {
+	var err error
+	// 根据配置的协议和地址，创建一个服务器监听器
+	ep.listener, err = net.Listen(ep.Config.Protocol, ep.Config.Addr)
+	if err != nil {
+		return err
+	}
+	// 打印服务器启动的信息
+	ep.Printf("starting server on :%s", ep.Config.Addr)
+	// 循环接受客户端的连接请求
+	for {
+		// 从监听器中获取一个客户端连接，返回连接对象和错误信息
+		conn, err := ep.listener.Accept()
+		if err != nil {
+			ep.Printf("accept:", err)
+			continue
+		}
+		// 打印客户端连接的信息
+		ep.Printf("new connection from:", conn.RemoteAddr().String())
+		// 启动一个协程处理客户端连接
+		go ep.handler(conn)
+	}
+}
+
+func (ep *Endpoint) Printf(format string, v ...interface{}) {
+	if ep.RuleConfig.Logger != nil {
+		ep.RuleConfig.Logger.Printf(format, v...)
+	}
+}
+
+func (ep *Endpoint) handler(conn net.Conn) {
+	h := ClientHandler{
+		endpoint: ep,
+		conn:     conn,
+	}
+	h.handler()
+}
+
+type ClientHandler struct {
+	endpoint *Endpoint
+	// 客户端连接对象
+	conn net.Conn
+	// 创建一个读取超时定时器，用于设置读取数据的超时时间，可以为0表示不设置超时
+	readTimeoutTimer *time.Timer
+}
+
+func (x *ClientHandler) handler() {
+	defer func() {
+		_ = x.conn.Close()
+		//捕捉异常
+		if e := recover(); e != nil {
+			x.endpoint.Printf("net handler err :%v\n %s", e, assert.CallerInfo())
+		}
+	}()
+	readTimeoutDuration := time.Duration(x.endpoint.Config.ReadTimeout+5) * time.Second
+	//读超时，断开连接
+	x.readTimeoutTimer = time.AfterFunc(readTimeoutDuration, func() {
+		if x.endpoint.Config.ReadTimeout > 0 {
+			x.onDisconnect()
+		}
+	})
+	// 创建一个缓冲读取器，用于读取客户端发送的数据
+	reader := bufio.NewReader(x.conn)
+	// 循环读取客户端发送的数据
+	for {
+		// 设置读取超时
+		if x.endpoint.Config.ReadTimeout > 0 {
+			err := x.conn.SetReadDeadline(time.Now().Add(readTimeoutDuration))
+			if err != nil {
+				x.onDisconnect()
+				break
+			}
+		}
+
+		// 读取一行数据，直到遇到\n或者\t\n为止
+		data, _, err := reader.ReadLine()
+
+		if err != nil && err.Error() != os.ErrDeadlineExceeded.Error() {
+			if e, ok := err.(*net.OpError); ok {
+				if e.Err != os.ErrDeadlineExceeded {
+					x.onDisconnect()
+					break
+				} else {
+					continue
+				}
+			} else {
+				x.onDisconnect()
+				break
+			}
+		}
+		//重置读超时定时器
+		if x.endpoint.Config.ReadTimeout > 0 {
+			x.readTimeoutTimer.Reset(readTimeoutDuration)
+		}
+		if string(data) == "ping" {
+			continue
+		}
+		// 创建一个交换对象，用于存储输入和输出的消息
+		exchange := &endpoint.Exchange{
+			In: &RequestMessage{
+				conn: x.conn,
+				body: data,
+			},
+			Out: &ResponseMessage{
+				log: func(format string, v ...interface{}) {
+					x.endpoint.Printf(format, v...)
+				},
+				conn: x.conn,
+			}}
+
+		msg := exchange.In.GetMsg()
+		// 把客户端连接的地址放到msg元数据中
+		msg.Metadata.PutValue("remoteAddr", x.conn.RemoteAddr().String())
+
+		// 匹配符合的路由，处理消息
+		for _, v := range x.endpoint.routers {
+			if v.regexp == nil || v.regexp.Match(data) {
+				x.endpoint.DoProcess(v.router, exchange)
+			}
+		}
+	}
+
+}
+
+func (x *ClientHandler) onDisconnect() {
+	if x.conn != nil {
+		_ = x.conn.Close()
+	}
+	if x.readTimeoutTimer != nil {
+		x.readTimeoutTimer.Stop()
+	}
+	x.endpoint.Printf("onDisconnect:" + x.conn.RemoteAddr().String())
+}
+
+//func (x *ClientHandler) onStop() {
+//	// 发送一个空结构体到chain
+//	x.stop <- struct{}{}
+//}
