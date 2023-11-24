aed84b82fd5262d4bdda2beaa5ea62fb37d0a7c5
rulego-team <rulego@outlook.com>
2023-11-24 11:58:22 +0800

    doc:add doc

--- rulego.go
+++ rulego.go
@@ -14,6 +14,95 @@
  * limitations under the License.
  */
 
+// Package rulego provides a lightweight, high-performance, embedded, orchestrable component-based rule engine.
+//
+// # Usage
+//
+// Implement your business requirements by configuring components in the rule chain, and support dynamic modification. Rule chain definition format:
+//
+//	{
+//		"ruleChain": {
+//			"id":"rule01"
+//		},
+//		"metadata": {
+//			"nodes": [
+//			],
+//			"connections": [
+//			]
+//		}
+//	}
+//
+// Example:
+//
+//	var ruleFile = `
+//	{
+//		"ruleChain": {
+//		"id":"rule02",
+//		"name": "test",
+//		"root": true
+//		},
+//		"metadata": {
+//		"nodes": [
+//			{
+//			"id": "s1",
+//			"type": "jsTransform",
+//			"name": "transform",
+//			"debugMode": true,
+//			"configuration": {
+//				"jsScript": "metadata['name']='test02';\n metadata['index']=22;\n msg['addField']='addValue2'; return {'msg':msg,'metadata':metadata,'msgType':msgType};"
+//				}
+//			},
+//			{
+//				"id": "s2",
+//				"type": "restApiCall",
+//				"name": "push data",
+//				"debugMode": true,
+//				"configuration": {
+//					"restEndpointUrlPattern": "http://192.168.1.1:9099/api/msg",
+//					"requestMethod": "POST",
+//					"maxParallelRequestsCount": 200
+//				}
+//			}
+//		],
+//		"connections": [
+//			{
+//				"fromId": "s1",
+//				"toId": "s2",
+//				"type": "Success"
+//			}
+//		]
+//		}
+//	}
+//	`
+//
+// Create Rule Engine Instance
+//
+//	ruleEngine, err := rulego.New("rule01", []byte(ruleFile))
+//
+// Define Message Metadata
+//
+//	metaData := types.NewMetadata()
+//	metaData.PutValue("productType", "test01")
+//
+// Define Message Payload And Type
+//
+//	msg := types.NewMsg(0, "TELEMETRY_MSG", types.JSON, metaData, "{\"temperature\":35}")
+//
+// Processing Message
+//
+//	ruleEngine.OnMsg(msg)
+//
+// Update Rule Chain
+//
+//	err := ruleEngine.ReloadSelf([]byte(ruleFile))
+//
+// Load Rule Chain Folder
+//
+//	err := ruleEngine.Load("./rulechains")
+//
+// Get Engine Instance
+//
+//	ruleEngine, ok := rulego.Get("rule01")
 package rulego
 
 import (
@@ -25,13 +114,13 @@ import (
 
 var DefaultRuleGo = &RuleGo{}
 
-//RuleGo 规则引擎实例池
+// RuleGo 规则引擎实例池
 type RuleGo struct {
 	ruleEngines sync.Map
 }
 
-//Load 加载指定文件夹及其子文件夹所有规则链配置（与.json结尾文件），到规则引擎实例池
-//规则链ID，使用规则链文件配置的ruleChain.id
+// Load 加载指定文件夹及其子文件夹所有规则链配置（与.json结尾文件），到规则引擎实例池
+// 规则链ID，使用规则链文件配置的ruleChain.id
 func (g *RuleGo) Load(folderPath string, opts ...RuleEngineOption) error {
 	if !strings.HasSuffix(folderPath, "*.json") && !strings.HasSuffix(folderPath, "*.JSON") {
 		if strings.HasSuffix(folderPath, "/") || strings.HasSuffix(folderPath, "\\") {
@@ -57,8 +146,8 @@ func (g *RuleGo) Load(folderPath string, opts ...RuleEngineOption) error {
 	return nil
 }
 
-//New 创建一个新的RuleEngine并将其存储在RuleGo规则链池中
-//如果指定id="",则使用规则链文件的ruleChain.id
+// New 创建一个新的RuleEngine并将其存储在RuleGo规则链池中
+// 如果指定id="",则使用规则链文件的ruleChain.id
 func (g *RuleGo) New(id string, rootRuleChainSrc []byte, opts ...RuleEngineOption) (*RuleEngine, error) {
 	if v, ok := g.ruleEngines.Load(id); ok {
 		return v.(*RuleEngine), nil
@@ -76,7 +165,7 @@ func (g *RuleGo) New(id string, rootRuleChainSrc []byte, opts ...RuleEngineOptio
 	}
 }
 
-//Get 获取指定ID规则引擎实例
+// Get 获取指定ID规则引擎实例
 func (g *RuleGo) Get(id string) (*RuleEngine, bool) {
 	v, ok := g.ruleEngines.Load(id)
 	if ok {
@@ -84,20 +173,18 @@ func (g *RuleGo) Get(id string) (*RuleEngine, bool) {
 	} else {
 		return nil, false
 	}
-
 }
 
-//Del 删除指定ID规则引擎实例
+// Del 删除指定ID规则引擎实例
 func (g *RuleGo) Del(id string) {
 	v, ok := g.ruleEngines.Load(id)
 	if ok {
 		v.(*RuleEngine).Stop()
 		g.ruleEngines.Delete(id)
 	}
-
 }
 
-//Stop 释放所有规则引擎实例
+// Stop 释放所有规则引擎实例
 func (g *RuleGo) Stop() {
 	g.ruleEngines.Range(func(key, value any) bool {
 		if item, ok := value.(*RuleEngine); ok {
@@ -108,8 +195,8 @@ func (g *RuleGo) Stop() {
 	})
 }
 
-//OnMsg 调用所有规则引擎实例处理消息
-//规则引擎实例池所有规则链都会去尝试处理该消息
+// OnMsg 调用所有规则引擎实例处理消息
+// 规则引擎实例池所有规则链都会去尝试处理该消息
 func (g *RuleGo) OnMsg(msg types.RuleMsg) {
 	g.ruleEngines.Range(func(key, value any) bool {
 		if item, ok := value.(*RuleEngine); ok {
@@ -119,34 +206,34 @@ func (g *RuleGo) OnMsg(msg types.RuleMsg) {
 	})
 }
 
-//Load 加载指定文件夹及其子文件夹所有规则链配置（与.json结尾文件），到规则引擎实例池
-//规则链ID，使用文件配置的 ruleChain.id
+// Load 加载指定文件夹及其子文件夹所有规则链配置（与.json结尾文件），到规则引擎实例池
+// 规则链ID，使用文件配置的 ruleChain.id
 func Load(folderPath string, opts ...RuleEngineOption) error {
 	return DefaultRuleGo.Load(folderPath, opts...)
 }
 
-//New 创建一个新的RuleEngine并将其存储在RuleGo规则链池中
+// New 创建一个新的RuleEngine并将其存储在RuleGo规则链池中
 func New(id string, rootRuleChainSrc []byte, opts ...RuleEngineOption) (*RuleEngine, error) {
 	return DefaultRuleGo.New(id, rootRuleChainSrc, opts...)
 }
 
-//Get 获取指定ID规则引擎实例
+// Get 获取指定ID规则引擎实例
 func Get(id string) (*RuleEngine, bool) {
 	return DefaultRuleGo.Get(id)
 }
 
-//Del 删除指定ID规则引擎实例
+// Del 删除指定ID规则引擎实例
 func Del(id string) {
 	DefaultRuleGo.Del(id)
 }
 
-//Stop 释放所有规则引擎实例
+// Stop 释放所有规则引擎实例
 func Stop() {
 	DefaultRuleGo.Stop()
 }
 
-//OnMsg 调用所有规则引擎实例处理消息
-//规则引擎实例池所有规则链都会去尝试处理该消息
+// OnMsg 调用所有规则引擎实例处理消息
+// 规则引擎实例池所有规则链都会去尝试处理该消息
 func OnMsg(msg types.RuleMsg) {
 	DefaultRuleGo.OnMsg(msg)
 }
