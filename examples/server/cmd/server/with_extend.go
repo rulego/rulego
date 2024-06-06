5c1fc51c990c0a17fd402b2fa1b165e5ae62296d
rulego-team <rulego@outlook.com>
2024-06-07 00:08:12 +0800

    Bump go version & rulego-components

--- examples/server/cmd/server/with_extend.go
+++ examples/server/cmd/server/with_extend.go
@@ -6,6 +6,7 @@ import (
 	//注册扩展组件库
 	// 使用`go build -tags with_extend .`把扩展组件编译到运行文件
 	_ "github.com/rulego/rulego-components/external/kafka"
+	_ "github.com/rulego/rulego-components/external/nats"
 	_ "github.com/rulego/rulego-components/external/redis"
 	_ "github.com/rulego/rulego-components/filter"
 	_ "github.com/rulego/rulego-components/transform"
