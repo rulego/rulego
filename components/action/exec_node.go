dc01296813cbf94eb4286754b39ec16906ef822d
rulego-team <rulego@outlook.com>
2024-06-23 12:25:19 +0800

    add exec node

---
+++ components/action/exec_node.go
@@ -0,0 +1,178 @@
+package action
+
+import (
+	"bytes"
+	"errors"
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/utils/maps"
+	"github.com/rulego/rulego/utils/str"
+	"io"
+	"os/exec"
+	"strings"
+)
+
+// ErrCmdNotAllowed 不允许执行的命令
+var ErrCmdNotAllowed = errors.New("cmd not allowed error")
+
+const (
+	// KeyExecNodeWhitelist ExecCommandNode node whitelist list
+	KeyExecNodeWhitelist = "execNodeWhitelist"
+	//KeyWorkDir cmd working directory
+	KeyWorkDir = "workDir"
+)
+
+func init() {
+	Registry.Add(&ExecCommandNode{})
+}
+
+// ExecCommandNodeConfiguration 节点配置
+type ExecCommandNodeConfiguration struct {
+	// Cmd 执行的命令
+	Cmd string
+	// Args 命令参数
+	Args []string
+	// Log 是否打印标准输出。true:命令标准输出会触发OnDebug函数输出
+	Log bool
+	//是否把标准输出到下一个节点
+	ReplaceData bool
+}
+
+// ExecCommandNode 执行本地命令，在白名单的命令才允许执行，通过config.Properties `key=execNodeWhitelist`，设置白名单，多个与`,`隔开
+// 示例：config.Properties.PutValue(KeyExecNodeWhitelist,"cd,ls,go")
+// 允许通过上一个节点通过元数据`key=workDir`，设置命令的执行目录，如：Metadata.PutValue("workDir","./data")
+type ExecCommandNode struct {
+	// 节点配置
+	Config ExecCommandNodeConfiguration
+	// 白名单命令列表
+	CommandWhitelist []string
+}
+
+// Type 组件类型
+func (x *ExecCommandNode) Type() string {
+	return "exec"
+}
+
+func (x *ExecCommandNode) New() types.Node {
+	return &ExecCommandNode{}
+}
+
+// Init 初始化
+func (x *ExecCommandNode) Init(ruleConfig types.Config, configuration types.Configuration) error {
+	if err := maps.Map2Struct(configuration, &x.Config); err != nil {
+		return err
+	}
+	x.CommandWhitelist = strings.Split(ruleConfig.Properties.GetValue(KeyExecNodeWhitelist), ",")
+	return nil
+}
+
+// OnMsg 处理消息
+func (x *ExecCommandNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) {
+	// 创建 metadata 的副本并添加消息内容
+	metadataCopy := msg.Metadata.Copy()
+	metadataCopy.PutValue("msg.data", msg.Data)
+	metadataCopy.PutValue("msg.type", msg.Type)
+
+	// 替换命令中的占位符
+	command := str.SprintfDict(x.Config.Cmd, metadataCopy.Values())
+
+	// 检查命令是否在白名单中
+	if !isCommandWhitelisted(command, x.CommandWhitelist) {
+		ctx.TellFailure(msg, ErrCmdNotAllowed)
+		return
+	}
+
+	// 替换参数中的占位符
+	var args []string
+	for _, arg := range x.Config.Args {
+		if !strings.HasPrefix(arg, "\"") {
+			v := strings.Split(arg, " ")
+			for _, item := range v {
+				args = append(args, str.SprintfDict(item, metadataCopy.Values()))
+			}
+		} else {
+			args = append(args, str.SprintfDict(arg, metadataCopy.Values()))
+		}
+	}
+
+	// 执行命令
+	cmd := exec.Command(command, args...)
+	// 设置命令的工作目录
+	cmd.Dir = msg.Metadata.GetValue(KeyWorkDir)
+	var stdoutBuf, stderrBuf bytes.Buffer
+	if x.Config.Log {
+		x.printLog(ctx, msg, cmd, &stdoutBuf, &stderrBuf)
+	} else if x.Config.ReplaceData {
+		cmd.Stdout = &stdoutBuf
+		cmd.Stderr = &stderrBuf
+	}
+
+	// 启动命令
+	if err := cmd.Start(); err != nil {
+		ctx.TellFailure(msg, err)
+		return
+	}
+	// 等待命令执行完成
+	if err := cmd.Wait(); err != nil {
+		ctx.TellFailure(msg, err)
+		return
+	}
+	if x.Config.ReplaceData {
+		msg.Data = stdoutBuf.String()
+	}
+	ctx.TellSuccess(msg)
+}
+
+// Destroy 销毁
+func (x *ExecCommandNode) Destroy() {
+}
+
+func (x *ExecCommandNode) printLog(ctx types.RuleContext, msg types.RuleMsg, cmd *exec.Cmd, bufOut *bytes.Buffer, bufErr *bytes.Buffer) {
+	// 启用日志记录
+	var chainId = ""
+	if ctx.RuleChain() != nil {
+		chainId = ctx.RuleChain().GetNodeId().Id
+	}
+	msgCopy := msg.Copy()
+	// 创建 DebugWriter 实例
+	debugWriter := &OnDebugWriter{
+		ctx:          ctx,
+		msg:          msgCopy,
+		relationType: "info",
+		chainId:      chainId,
+	}
+	errWriter := &OnDebugWriter{
+		ctx:          ctx,
+		msg:          msgCopy,
+		relationType: "error",
+		chainId:      chainId,
+	}
+	// 将命令的输出重定向到 DebugWriter
+	cmd.Stdout = io.MultiWriter(bufOut, debugWriter)
+	cmd.Stderr = io.MultiWriter(bufErr, errWriter)
+}
+
+// isCommandWhitelisted 检查命令是否在白名单中
+func isCommandWhitelisted(command string, whitelist []string) bool {
+	for _, whitelistedCommand := range whitelist {
+		if command == whitelistedCommand {
+			return true
+		}
+	}
+	return false
+}
+
+type OnDebugWriter struct {
+	ctx          types.RuleContext
+	msg          types.RuleMsg
+	relationType string
+	chainId      string
+}
+
+func (w *OnDebugWriter) Write(p []byte) (n int, err error) {
+	// 将接收到的数据转换为字符串
+	w.msg.Data = string(p)
+	// 调用 OnDebug 方法来记录日志
+	w.ctx.Config().OnDebug(w.chainId, types.Log, w.ctx.GetSelfId(), w.msg, w.relationType, nil)
+	// 返回写入的字节数和nil错误
+	return len(p), nil
+}
---
+++ components/action/exec_node_test.go
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2023 The RuleGo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package action
+
+import (
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/test"
+	"github.com/rulego/rulego/test/assert"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestTemplateNode(t *testing.T) {
+	var targetNodeType = "exec"
+
+	t.Run("NewNode", func(t *testing.T) {
+		test.NodeNew(t, targetNodeType, &ExecCommandNode{}, types.Configuration{}, Registry)
+	})
+
+	t.Run("InitNode", func(t *testing.T) {
+		test.NodeInit(t, targetNodeType, types.Configuration{
+			"cmd":  "ls",
+			"args": []string{"./data"},
+		}, types.Configuration{
+			"cmd":  "ls",
+			"args": []string{"./data"},
+		}, Registry)
+	})
+
+	t.Run("OnMsg", func(t *testing.T) {
+		metaData := types.BuildMetadata(make(map[string]string))
+		metaData.PutValue("productType", "test")
+		metaData.PutValue("dir", ".")
+		msg := test.Msg{
+			Id:         "226a05f1-9464-43b6-881e-b1629f1b030d",
+			Ts:         1719024872741,
+			MetaData:   metaData,
+			MsgType:    "ACTIVITY_EVENT",
+			Data:       "{\"name\":\"aa\",\"temperature\":60,\"humidity\":30}",
+			AfterSleep: time.Millisecond * 200,
+		}
+
+		count := int32(0)
+		config := types.NewConfig()
+		config.Properties.PutValue(KeyExecNodeWhitelist, "ls,cd")
+		config.OnDebug = func(ruleChainId string, flowType string, nodeId string, msg types.RuleMsg, relationType string, err error) {
+			assert.Equal(t, types.Log, flowType)
+			assert.Equal(t, "info", relationType)
+			atomic.AddInt32(&count, 1)
+		}
+		var data1, data2 string
+		var nodeList = []test.NodeAndCallback{
+			{
+				Node: test.InitNodeByConfig(types.NewConfig(), targetNodeType, types.Configuration{
+					"cmd":  "ls",
+					"args": []string{"."},
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, ErrCmdNotAllowed.Error(), err.Error())
+					assert.Equal(t, types.Failure, relationType)
+				},
+			},
+			{
+				Node: test.InitNodeByConfig(config, targetNodeType, types.Configuration{
+					"cmd":         "ls",
+					"args":        []string{"xx"},
+					"replaceData": true,
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, types.Failure, relationType)
+				},
+			},
+			{
+				Node: test.InitNodeByConfig(config, targetNodeType, types.Configuration{
+					"cmd":  "ls",
+					"args": []string{"."},
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, "{\"name\":\"aa\",\"temperature\":60,\"humidity\":30}", msg.Data)
+					assert.Equal(t, types.Success, relationType)
+				},
+			},
+			{
+				Node: test.InitNodeByConfig(config, targetNodeType, types.Configuration{
+					"cmd":         "ls",
+					"args":        []string{"${dir}"},
+					"log":         true,
+					"replaceData": true,
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.NotEqual(t, "{\"name\":\"aa\",\"temperature\":60,\"humidity\":30}", msg.Data)
+					assert.Equal(t, types.Success, relationType)
+				},
+			},
+			{
+				Node: test.InitNodeByConfig(config, targetNodeType, types.Configuration{
+					"cmd":         "ls",
+					"args":        []string{"${dir} -l"},
+					"replaceData": true,
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.NotEqual(t, "{\"name\":\"aa\",\"temperature\":60,\"humidity\":30}", msg.Data)
+					assert.Equal(t, types.Success, relationType)
+					data1 = msg.Data
+				},
+			},
+			{
+				Node: test.InitNodeByConfig(config, targetNodeType, types.Configuration{
+					"cmd":         "ls",
+					"args":        []string{"${dir}", "-l"},
+					"replaceData": true,
+				}, Registry),
+				MsgList: []test.Msg{msg},
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.NotEqual(t, "{\"name\":\"aa\",\"temperature\":60,\"humidity\":30}", msg.Data)
+					assert.Equal(t, types.Success, relationType)
+					data2 = msg.Data
+				},
+			},
+		}
+		for _, item := range nodeList {
+			test.NodeOnMsgWithChildrenAndConfig(t, config, item.Node, item.MsgList, item.ChildrenNodes, item.Callback)
+		}
+		time.Sleep(time.Second)
+		assert.Equal(t, int32(1), count)
+		assert.Equal(t, data1, data2)
+	})
+}
