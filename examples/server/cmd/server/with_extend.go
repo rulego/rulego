c9753588651efea50509d5325c0ecebaacad1197
rulego-team <rulego@outlook.com>
2024-07-04 22:28:16 +0800

    feat:server add extend endpoint

--- examples/server/cmd/server/with_extend.go
+++ examples/server/cmd/server/with_extend.go
@@ -3,8 +3,10 @@
 package main
 
 import (
-	//注册扩展组件库
+	// 注册扩展组件库
 	// 使用`go build -tags with_extend .`把扩展组件编译到运行文件
+	_ "github.com/rulego/rulego-components/endpoint/kafka"
+	_ "github.com/rulego/rulego-components/endpoint/nats"
 	_ "github.com/rulego/rulego-components/external/kafka"
 	_ "github.com/rulego/rulego-components/external/nats"
 	_ "github.com/rulego/rulego-components/external/redis"
