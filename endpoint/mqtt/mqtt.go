b5171198c52a6e6ac9ed5bd6acd79f4ce2799098
rulego-team <rulego@outlook.com>
2024-07-31 21:05:49 +0800

    refactor:modify response topic

--- endpoint/mqtt/mqtt.go
+++ endpoint/mqtt/mqtt.go
@@ -34,6 +34,12 @@ import (
 
 // Type 组件类型
 const Type = types.EndpointTypePrefix + "mqtt"
+const (
+	// KeyResponseTopic 响应主题metadataKey
+	KeyResponseTopic = "responseTopic"
+	// KeyResponseQos 响应Qos metadataKey
+	KeyResponseQos = "responseQos"
+)
 
 // Endpoint 别名
 type Endpoint = Mqtt
@@ -158,12 +164,25 @@ func (r *ResponseMessage) GetMsg() *types.RuleMsg {
 func (r *ResponseMessage) SetStatusCode(statusCode int) {
 }
 
+// 从msg.Metadata或者响应头获取
+func (r *ResponseMessage) getMetadataValue(metadataName, headerName string) string {
+	var v string
+	if r.GetMsg() != nil {
+		metadata := r.GetMsg().Metadata
+		v = metadata.GetValue(metadataName)
+	}
+	if v == "" {
+		return r.Headers().Get(headerName)
+	} else {
+		return v
+	}
+}
+
 func (r *ResponseMessage) SetBody(body []byte) {
 	r.body = body
-
-	topic := r.Headers().Get("topic")
+	topic := r.getMetadataValue(KeyResponseTopic, KeyResponseTopic)
 	if topic != "" {
-		qosStr := r.Headers().Get("qos")
+		qosStr := r.getMetadataValue(KeyResponseQos, KeyResponseQos)
 		qos := byte(0)
 		if qosStr != "" {
 			qosInt, _ := strconv.Atoi(qosStr)
