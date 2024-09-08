fbecfe1cfe527a9ad8c1f6cd008961da8e805d4c
rulego-team <rulego@outlook.com>
2024-09-08 11:12:56 +0800

    feat:add ref node

---
+++ components/flow/ref_node_test.go
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2023 The RuleGo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flow
+
+import (
+	"github.com/rulego/rulego/api/types"
+	"github.com/rulego/rulego/test"
+	"github.com/rulego/rulego/test/assert"
+	"testing"
+	"time"
+)
+
+func TestRefNode(t *testing.T) {
+	var targetNodeType = "ref"
+
+	t.Run("NewNode", func(t *testing.T) {
+		test.NodeNew(t, targetNodeType, &RefNode{}, types.Configuration{}, Registry)
+	})
+
+	t.Run("InitNode", func(t *testing.T) {
+		test.NodeInit(t, targetNodeType, types.Configuration{}, types.Configuration{}, Registry)
+	})
+
+	t.Run("DefaultConfig", func(t *testing.T) {
+		test.NodeInit(t, targetNodeType, types.Configuration{}, types.Configuration{}, Registry)
+	})
+
+	t.Run("OnMsg", func(t *testing.T) {
+		node1, err := test.CreateAndInitNode(targetNodeType, types.Configuration{}, Registry)
+		assert.Nil(t, err)
+
+		node2, err := test.CreateAndInitNode(targetNodeType, types.Configuration{
+			"targetId": "chain01:node01",
+		}, Registry)
+		assert.Equal(t, "chain01", node2.(*RefNode).chainId)
+		assert.Equal(t, "node01", node2.(*RefNode).nodeId)
+
+		node3, err := test.CreateAndInitNode(targetNodeType, types.Configuration{
+			"targetId": "node01",
+		}, Registry)
+		assert.Equal(t, "", node3.(*RefNode).chainId)
+		assert.Equal(t, "node01", node3.(*RefNode).nodeId)
+
+		metaData := types.BuildMetadata(make(map[string]string))
+		metaData.PutValue("productType", "test")
+		msgList := []test.Msg{
+			{
+				MetaData:   metaData,
+				MsgType:    "ACTIVITY_EVENT2",
+				Data:       "{\"temperature\":60}",
+				AfterSleep: time.Millisecond * 200,
+			},
+		}
+
+		var nodeList = []test.NodeAndCallback{
+			{
+				Node:    node1,
+				MsgList: msgList,
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, types.Failure, relationType)
+				},
+			},
+			{
+				Node:    node2,
+				MsgList: msgList,
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, types.Failure, relationType)
+				},
+			},
+			{
+				Node:    node3,
+				MsgList: msgList,
+				Callback: func(msg types.RuleMsg, relationType string, err error) {
+					assert.Equal(t, types.Failure, relationType)
+				},
+			},
+		}
+		for _, item := range nodeList {
+			test.NodeOnMsgWithChildren(t, item.Node, item.MsgList, item.ChildrenNodes, item.Callback)
+		}
+	})
+}
