205a688974bf4ec509412050b37e6475e3ffd245
rulego-team <rulego@outlook.com>
2024-10-29 21:27:18 +0800

    feat(server):add mongdb node

--- examples/server/cmd/server/with_extend.go
+++ examples/server/cmd/server/with_extend.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/rulego/rulego-components/endpoint/redis_stream"
 	_ "github.com/rulego/rulego-components/external/grpc" //编译后文件大约增加7M
 	_ "github.com/rulego/rulego-components/external/kafka"
+	_ "github.com/rulego/rulego-components/external/mongodb"
 	_ "github.com/rulego/rulego-components/external/nats"
 	_ "github.com/rulego/rulego-components/external/opengemini"
 	_ "github.com/rulego/rulego-components/external/rabbitmq"
