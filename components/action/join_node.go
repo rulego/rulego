e5951db6e2e9ce126d21ed4bb10c0a9bca2ed0fc
rulego-team <rulego@outlook.com>
2025-06-25 22:21:22 +0800

    refactor:Roll back join node implementation

--- components/action/join_node.go
+++ components/action/join_node.go
@@ -22,12 +22,11 @@ package action
 //	"type": "join",
 //	"name": "join",
 //	"configuration": {
-//		"timeout": 10
+//	 }
 //	}
 //}
 import (
 	"context"
-	"sync"
 	"time"
 
 	"github.com/rulego/rulego/api/types"
@@ -45,12 +44,9 @@ type JoinNodeConfiguration struct {
 }
 
 // JoinNode 合并多个异步节点执行结果
-// 内存优化版本：减少不必要的拷贝、使用对象池复用slice
 type JoinNode struct {
 	//节点配置
 	Config JoinNodeConfiguration
-	//消息缓冲池，复用slice减少GC压力
-	msgBufferPool sync.Pool
 }
 
 // Type 组件类型
@@ -59,38 +55,17 @@ func (x *JoinNode) Type() string {
 }
 
 func (x *JoinNode) New() types.Node {
-	node := &JoinNode{
-		Config: JoinNodeConfiguration{},
-	}
-	// 初始化对象池
-	node.msgBufferPool = sync.Pool{
-		New: func() interface{} {
-			// 预分配合理大小的slice，减少扩容开销
-			return make([]types.WrapperMsg, 0, 8)
-		},
-	}
-	return node
+	return &JoinNode{Config: JoinNodeConfiguration{}}
 }
 
 // Init 初始化
 func (x *JoinNode) Init(_ types.Config, configuration types.Configuration) error {
-	err := maps.Map2Struct(configuration, &x.Config)
-
-	// 确保对象池已初始化
-	if x.msgBufferPool.New == nil {
-		x.msgBufferPool = sync.Pool{
-			New: func() interface{} {
-				return make([]types.WrapperMsg, 0, 8)
-			},
-		}
-	}
-
-	return err
+	return maps.Map2Struct(configuration, &x.Config)
 }
 
-// OnMsg processes the message with memory optimization.
+// OnMsg processes the message.
 func (x *JoinNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) {
-	c := make(chan bool, 1)
+	c := make(chan struct{}, 1)
 	var chanCtx context.Context
 	var cancel context.CancelFunc
 	if x.Config.Timeout > 0 {
@@ -98,88 +73,28 @@ func (x *JoinNode) OnMsg(ctx types.RuleContext, msg types.RuleMsg) {
 	} else {
 		chanCtx, cancel = context.WithCancel(ctx.GetContext())
 	}
-
 	defer cancel()
 
-	// 延迟拷贝：只有在需要修改消息时才进行拷贝
-	var wrapperMsg types.RuleMsg
+	var wrapperMsg = msg.Copy()
 
 	ok := ctx.TellCollect(msg, func(msgList []types.WrapperMsg) {
-		// 现在才进行消息拷贝
-		wrapperMsg = msg.Copy()
-
-		// 内存优化的消息处理
-		x.processMessagesOptimized(msgList, &wrapperMsg)
-		c <- true
+		wrapperMsg.SetDataType(types.JSON)
+		wrapperMsg.SetData(str.ToString(filterEmptyAndRemoveMeta(msgList)))
+		mergeMetadata(msgList, &wrapperMsg)
+		select {
+		case c <- struct{}{}:
+		default: // 防止阻塞
+		}
 	})
-
 	if ok {
-		// 等待执行结束或者超时
 		select {
 		case <-chanCtx.Done():
-			// 超时时才创建拷贝
-			if wrapperMsg.Id == "" {
-				wrapperMsg = msg.Copy()
-			}
 			ctx.TellFailure(wrapperMsg, chanCtx.Err())
-		case _ = <-c:
+		case <-c:
 			ctx.TellSuccess(wrapperMsg)
 		}
 	}
 }
 
-// processMessagesOptimized 内存优化的消息处理方法
-func (x *JoinNode) processMessagesOptimized(msgList []types.WrapperMsg, wrapperMsg *types.RuleMsg) {
-	// 从对象池获取缓冲区
-	filteredMsgs := x.msgBufferPool.Get().([]types.WrapperMsg)
-	filteredMsgs = filteredMsgs[:0] // 重置长度但保留容量
-
-	defer func() {
-		// 归还到对象池前清理引用，防止内存泄漏
-		for i := range filteredMsgs {
-			filteredMsgs[i] = types.WrapperMsg{}
-		}
-		x.msgBufferPool.Put(filteredMsgs)
-	}()
-
-	// 内存优化的过滤和元数据合并
-	x.filterAndMergeOptimized(msgList, &filteredMsgs, wrapperMsg)
-
-	// 设置处理后的数据
-	wrapperMsg.DataType = types.JSON
-	wrapperMsg.SetData(str.ToString(filteredMsgs))
-}
-
-// filterAndMergeOptimized 内存优化的过滤和元数据合并
-func (x *JoinNode) filterAndMergeOptimized(msgList []types.WrapperMsg, filteredMsgs *[]types.WrapperMsg, wrapperMsg *types.RuleMsg) {
-	// 预分配元数据映射的合理大小
-	metadataMap := make(map[string]string, len(msgList)*2)
-
-	for _, msg := range msgList {
-		if msg.NodeId != "" {
-			// 创建消息副本，但清理元数据以减少内存占用
-			msgCopy := msg
-			if msgCopy.Msg.Metadata != nil {
-				msgCopy.Msg.Metadata.Clear()
-			}
-			*filteredMsgs = append(*filteredMsgs, msgCopy)
-
-			// 高效的元数据合并：只合并成功的消息 - use zero-copy ForEach
-			if msg.Err == "" && msg.Msg.Metadata != nil {
-				msg.Msg.Metadata.ForEach(func(k, v string) bool {
-					metadataMap[k] = v
-					return true // continue iteration
-				})
-			}
-		}
-	}
-
-	// 批量设置元数据，减少锁开销
-	if len(metadataMap) > 0 {
-		wrapperMsg.Metadata.ReplaceAll(metadataMap)
-	}
-}
-
 func (x *JoinNode) Destroy() {
-	// 清理对象池（如果需要）
 }
