aca8906b07b7cf6a622feacb2003dd2cfbea437f
rulego-team <rulego@outlook.com>
2026-08-02 11:01:06 +0800

    feat(server): register all IoT collection, persistence and transform components

--- server/cmd/server/with_iot.go
+++ server/cmd/server/with_iot.go
@@ -8,20 +8,40 @@ import (
 	"time"
 
 	"github.com/rulego/rulego-components-iot/external/opcua"
-	opcuaClient "github.com/rulego/rulego-components-iot/pkg/opcua_client"
 	"github.com/rulego/rulego-components-iot/external/serial"
+	opcuaClient "github.com/rulego/rulego-components-iot/pkg/opcua_client"
+	"github.com/rulego/rulego/api/types"
 	"github.com/rulego/rulego/server/internal/registry"
 	"github.com/rulego/rulego/utils/maps"
 
 	// IoT Endpoint
+	_ "github.com/rulego/rulego-components-iot/endpoint/hj212"
+	_ "github.com/rulego/rulego-components-iot/endpoint/modbus_server"
 	_ "github.com/rulego/rulego-components-iot/endpoint/opcua"
 	_ "github.com/rulego/rulego-components-iot/endpoint/snmp"
 
-	// IoT External
+	// IoT External（采集/落盘/通用节点，blank import 触发各包 init 注册）
+	_ "github.com/rulego/rulego-components-iot/external/deviceio"
+	_ "github.com/rulego/rulego-components-iot/external/dlt645"
 	_ "github.com/rulego/rulego-components-iot/external/eip"
+	_ "github.com/rulego/rulego-components-iot/external/fins"
+	_ "github.com/rulego/rulego-components-iot/external/iec104"
+	_ "github.com/rulego/rulego-components-iot/external/influxdb"
+	_ "github.com/rulego/rulego-components-iot/external/mc"
 	_ "github.com/rulego/rulego-components-iot/external/modbus"
+	_ "github.com/rulego/rulego-components-iot/external/opengemini"
+	_ "github.com/rulego/rulego-components-iot/external/promremote"
 	_ "github.com/rulego/rulego-components-iot/external/s7"
 	_ "github.com/rulego/rulego-components-iot/external/snmp"
+	_ "github.com/rulego/rulego-components-iot/external/tdengine"
+	_ "github.com/rulego/rulego-components-iot/external/timescaledb"
+	_ "github.com/rulego/rulego-components-iot/external/tsdb"
+
+	// IoT Transform
+	_ "github.com/rulego/rulego-components-iot/transform/iot_to_series"
+
+	// IoT Action
+	_ "github.com/rulego/rulego-components-iot/action/control"
 )
 
 // browseTimeout 单次在线浏览的最长耗时(连接+遍历)
@@ -66,13 +86,18 @@ func opcuaBrowseHandler(params map[string]interface{}) (interface{}, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), browseTimeout)
 	defer cancel()
 
-	holder := opcuaClient.DefaultHolder(cfg)
+	holder := opcuaClient.DefaultHolder(cfg, types.DefaultLogger())
 	holder.Ctx = ctx
 	client, err := holder.NewOpcUaClient()
 	if err != nil {
 		return nil, err
 	}
-	defer client.Close(ctx)
+	// Close 用独立短超时 ctx，避免复用可能已到期的 browse ctx 导致服务端 session 未干净关闭
+	defer func() {
+		closeCtx, closeCancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer closeCancel()
+		_ = client.Close(closeCtx)
+	}()
 	return opcuaClient.Browse(ctx, client, strParam(params, "nodeId"), intParam(params, "depth"))
 }
 
